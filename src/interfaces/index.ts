d3852cbf62b76493b03bb6d84b34b33ba736950b
Nileshdcool <neil.sukalikar@gmail.com>
2025-04-17 06:04:09 +0200

    fix: update version in package.json to 1.0.2 and format interface definitions in index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,12 +1,23 @@
 export interface StrapiUser {
-    email: string | null;
-    id: string | null;
-    provider: string | null;
-    username: string | null;
-    blocked: boolean;
-    confirmed: boolean;
-    createdAt: string;
-    documentId: string;
-    publishedAt: string;
-    updatedAt: string;
-  }
+  email: string | null;
+  id: string | null;
+  provider: string | null;
+  username: string | null;
+  blocked: boolean;
+  confirmed: boolean;
+  createdAt: string;
+  documentId: string;
+  publishedAt: string;
+  updatedAt: string;
+}
+
+export interface IEducation {
+  id?: number;
+  documentId?: string;
+  degree: string;
+  school: string;
+  location: string;
+  from: string;
+  to: string;
+  achievements: string[];
+}
