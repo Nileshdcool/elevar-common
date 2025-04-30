d36b4f329bd0e30c071626be6d28800f16b9a924
Sachin Bangar <sbangar07@gmail.com>
2025-04-30 14:55:16 +0530

    interface for jobs

--- src/interfaces/job.interface.ts
+++ src/interfaces/job.interface.ts
@@ -25,3 +25,56 @@ export interface IJobApplication {
     link: string;
   }
   
+  export interface IJobApplicationSummary {
+    id: number;
+    totalApplied: number;
+    jobPlatform: string;
+    userEmail: string;
+    resume: string;
+    applicationCount: number;
+    skills: string[];
+    link: string;
+  }
+  
+  export interface IJobListing {
+    company: string;
+    title: string;
+    resume: string;
+    date: string; // ISO date string
+    link: string;
+  }
+
+  export interface Job {
+    JobTitle: string;
+    Company: string;
+    createdAt: string;  
+    portal?: {
+      name?: string;
+      url?: string;
+    };
+    // Other job fields
+  }
+
+  export interface PopulatedUpload {
+    id: string | number;  
+    FileId?: string;
+    UserEmail?: string;
+    user_resume_profile?: {
+        skills: Skill[];  
+      };
+    jobs?: {
+      portal?: {
+        name?: string;
+        url?: string;
+      };
+    JobTitle: string;
+    Company: string;
+    createdAt: string;  // Assuming it's a Date string
+    JobUrl:string;
+    }[];
+  }
+
+  export interface Skill {
+    skill: string;
+    years: string;
+  }
